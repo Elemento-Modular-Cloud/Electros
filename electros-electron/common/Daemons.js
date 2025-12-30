3b4a44811c2df801b58f5201d492422749e9fdc1
srobaldo <srobaldo@elemento.cloud>
2025-12-30 16:04:21 +0100

    Upgraded daemon process handling

---
+++ electros-electron/common/Daemons.js
@@ -0,0 +1,97 @@
+import { app } from "electron";
+import path from "path";
+import fs from "fs";
+import { spawn } from "child_process";
+
+
+export class Daemons {
+    static _Process = null;
+    static _Ports = {};
+
+    static stdoutBuffer = "";
+    static stderrBuffer = "";
+    static BUFFER_SIZE = 1024;
+    static flushInterval = null;
+
+    static DataUpdateStdoutHook = null;
+    static DataUpdateStderrHook = null;
+    static DataUpdateCriticalHook = null;
+
+    static Launch(platform, __dirname) {
+        const execPath = Daemons._GetCommand(platform, __dirname);
+
+        Daemons._Process = spawn(
+            execPath, [], {
+                env: { ...process.env, GUI_APP: '1' },
+                stdio: ['pipe', 'pipe', 'pipe'],
+                detached: false,
+            }
+        );
+
+        console.trace(`Daemons process has been launched with PID`);
+        console.dir(Daemons._Process);
+
+        Daemons._Process.stdout.on("data", (data) => {
+            this.stdoutBuffer += data.toString();
+
+            if (this.stdoutBuffer.length > Daemons.BUFFER_SIZE) {
+                if (Daemons.DataUpdateStdoutHook) { Daemons.DataUpdateStdoutHook(data); }
+                Daemons.stdoutBuffer = "";
+            }
+        });
+
+        Daemons._Process.stderr.on("data", (data) => {
+            this.stderrBuffer += data.toString();
+
+            if (this.stderrBuffer.length > Daemons.BUFFER_SIZE) {
+                if (Daemons.DataUpdateStderrHook) { Daemons.DataUpdateStderrHook(data); }
+                Daemons.stderrBuffer = "";
+            }
+        });
+
+        Daemons._Process.on("error", (data) => {
+            if (Daemons.DataUpdateCriticalHook) { Daemons.DataUpdateCriticalHook(data); }
+        });
+    }
+
+    static Terminate() {
+        let r = true;
+        if (Daemons._Process) {
+            if (!Daemons._Process.killed) {
+                r = Daemons._Process.kill();
+            }
+            Daemons._Process = null;
+        }
+
+        return r;
+    }
+
+    static _GetCommand(platform, __dirname) {
+        const baseDir = app.isPackaged ? process.resourcesPath : path.join(__dirname, '..');
+        const daemonsPath = path.join(baseDir, 'electros-daemons', platform.os, platform.arch);
+
+        let daemonsCmd = '';
+
+        if (platform.os === 'mac') {
+            daemonsCmd = path.join(daemonsPath, "elemento_client_daemons.app/Contents/MacOS/elemento_client_daemons");
+        } else if (platform.os === 'linux') {
+            if (platform.arch === 'arm64') {
+                daemonsCmd = path.join(daemonsPath, `elemento_daemons_linux_arm`);
+            } else {
+                daemonsCmd = path.join(daemonsPath, `elemento_daemons_linux_x86`);
+            }
+        } else if (platform.os === 'win') {
+            if (platform.arch === 'x64' || platform.arch === 'x86') {
+                daemonsCmd = path.join(daemonsPath, `elemento_daemons_windows_x86.exe`);
+                if (!fs.existsSync(daemonsCmd)) {
+                    daemonsCmd = path.join(daemonsPath, `elemento_daemons_windows_x64.exe`);
+                }
+            } else {
+                daemonsCmd = path.join(daemonsPath, `elemento_daemons_windows_arm64.exe`);
+            }
+        }
+
+        return daemonsCmd;
+    }
+}
+
